d99ab460c71dd459585f9277852ad0daefe0964d
Harshit Pradhan <hpradhan020@gmail.com>
2023-11-16 01:33:31 +0530

    Feat: Dashbaord page component

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Dashboard;
