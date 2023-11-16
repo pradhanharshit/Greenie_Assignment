8d4b74ef915cdfaffe966491fd36bcad3de922cb
Harshit Pradhan <hpradhan020@gmail.com>
2023-11-16 20:45:17 +0530

    Feat: Search bar

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="text-center">
+      <input
+        type="text"
+        placeholder="Search User"
+        className="border-2 border-black rounded-md p-1"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
