b55e0f20991e5f4a9b17daf1084e6583a3b68b2b
Harshit Pradhan <hpradhan020@gmail.com>
2023-11-16 20:46:38 +0530

    Feat: added tabs, search bar & users data table

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/Navbar/Navbar";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import TabButtons from "@/components/TabButtons/TabButtons";
+import UsersTable from "@/components/UsersTable/UsersTable";
 import React from "react";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
+      <TabButtons />
+      <SearchBar />
+      <UsersTable />
     </div>
   );
 };
