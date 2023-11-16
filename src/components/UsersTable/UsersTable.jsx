472fd4e20065f73243b88f2816b3ed95503bd1d9
Harshit Pradhan <hpradhan020@gmail.com>
2023-11-16 20:45:57 +0530

    Feat: User Data table

---
+++ src/components/UsersTable/UsersTable.jsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Card, Typography } from "@material-tailwind/react";
+import UsersData from "../../Data/UsersData.json";
+
+const TABLE_HEAD = ["Username", "Email", "Phone", "ID", "Creation Date"];
+
+const UsersTable = () => {
+  return (
+    <div className="p-10">
+      <Card className="h-[400px] w-full overflow-scroll">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {UsersData?.map((userData, index) => {
+              const isLast = index === UsersData?.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
+
+              return (
+                <tr key={index}>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {userData.firstName + " " + userData.lastName}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {userData.email}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {userData.phone}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      className="font-medium"
+                    >
+                      {userData.id}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      className="font-medium"
+                    >
+                      {userData.creationDate}
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
+    </div>
+  );
+};
+
+export default UsersTable;
