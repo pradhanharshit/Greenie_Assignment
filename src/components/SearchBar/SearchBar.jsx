f5ae45e0c77c99564e44377b17356f67b28ce3bc
Harshit Pradhan <hpradhan020@gmail.com>
2023-11-17 17:55:28 +0530

    Feat: added search functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ search, setSearch }) => {
   return (
     <div className="text-center">
+      <p className="font-bold text-xl mb-5">User Details</p>
       <input
         type="text"
         placeholder="Search User"
         className="border-2 border-black rounded-md p-1"
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
       />
     </div>
   );
